c35c8931b1199986a83c99d849d850b39a5ca0e6
stephenoseiowusu <stephenoseiowusu.jobs@gmail.com>
2020-10-02 12:17:47 -0400

    3 files modified

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Import stylesheets
 import './style.css';
-
+import 'semantic-ui-css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 //appDiv.innerHTML = `<h1>JS Starter</h1>`;
